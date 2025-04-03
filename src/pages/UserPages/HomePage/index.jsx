790b52cb9241428d55e381a93eb4e6a02195deb3
ZakirAliyev <aliyevzakir814@gmail.com>
2025-04-03 18:28:37 +0400

    SSS

--- src/pages/UserPages/HomePage/index.jsx
+++ src/pages/UserPages/HomePage/index.jsx
@@ -1,10 +1,18 @@
 import Navbar from "../../../components/UserComponents/Navbar/index.jsx";
 import Footer from "../../../components/UserComponents/Footer/index.jsx";
+import ProjectsCard from "../../../components/UserComponents/ProjectsCard/index.jsx";
 
 function HomePage() {
     return (
         <section id={"homePage"}>
             <Navbar/>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <ProjectsCard/>
+                    <ProjectsCard/>
+                    <ProjectsCard/>
+                </div>
+            </div>
             <Footer/>
         </section>
     );
